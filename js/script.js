52e234b3691e2267114261dfa56d9fbf0bf46f92
Ivan <si28t36@gmail.com>
2020-07-23 23:45:29 +0300

    fix-slider

--- js/script.js
+++ js/script.js
@@ -13,3 +13,32 @@ $('.open-nav-btn').on('touchstart',function(evt) {
     $('#line-2').toggleClass('open-nav-btn__line--2');
     $('#line-3').toggleClass('open-nav-btn__line--3');
 });
+
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+  };
+
+$('.services__arrow--next').click(function (evt) {
+    evt.preventDefault();
+    let randomOrder1 = getRandomInt(2, 49);
+    let randomOrder2 = getRandomInt(2, 49);
+    let randomOrder3 = getRandomInt(2, 49);
+    let randomOrder4 = getRandomInt(2, 49);
+    $('.services__cards-link--1').css('order',randomOrder1);
+    $('.services__cards-link--2').css('order',randomOrder2);
+    $('.services__cards-link--3').css('order',randomOrder3);
+    $('.services__cards-link--4').css('order',randomOrder4);
+});
+
+$('.services__arrow--back').click(function (evt) {
+    evt.preventDefault();
+    let randomOrder1 = getRandomInt(2, 49);
+    let randomOrder2 = getRandomInt(2, 49);
+    let randomOrder3 = getRandomInt(2, 49);
+    let randomOrder4 = getRandomInt(2, 49);
+    $('.services__cards-link--1').css('order',randomOrder1);
+    $('.services__cards-link--2').css('order',randomOrder2);
+    $('.services__cards-link--3').css('order',randomOrder3);
+    $('.services__cards-link--4').css('order',randomOrder4);
+});
